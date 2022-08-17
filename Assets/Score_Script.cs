5b171c3def8c6ca38eb86e2f163d98d26ccb6958
Animated <chrisjnassif@gmail.com>
2022-08-17 18:33:14 -0400

    Added a lot of UI stuff in particular a scoring system

---
+++ Assets/Score_Script.cs
@@ -0,0 +1,61 @@
+// using System.Collections;
+// using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+//helper class (mostly for organization) that controls the special effects that the score text can go through depending on the situation
+public class Score_Effects {
+
+    public TextMeshProUGUI textMeshProUGUI; //same one as in the Score_Script
+
+    public Score_Effects(TextMeshProUGUI textMeshProUGUI_){
+
+        textMeshProUGUI = textMeshProUGUI_;
+    }
+
+    public void popScore(){} //makes the score pop out probably would be called when the player drops off children
+    public void enterDangerMode(){} //makes the text red and maybe makes it blink in someway when the score is beneath a certain threshold (maybe?)
+    public void enlargenScore(int score){} //makes the text slighty bigger depending on how large the score is. The bigger the score, the bigger the score's text to emphasize having a large score
+
+}
+
+
+public class Score_Script : MonoBehaviour
+{
+    private uint score;
+
+    public float timeMultiplier; //set in the editor and SHOULD be tweeked
+    public float childrenMultiplier; //set in the editor and SHOULD be tweeked
+    public float initialScore; //set in the editor and SHOULD be tweeked
+
+    private float scoreAdder; //score that you would need to add to start with the initial score that is desired
+
+    public TextMeshProUGUI scoreText;
+
+    public Score_Effects scoreEffects;
+
+    //this is called in start so that framesLeft is already initialized since Start is called after Awake
+    void Start(){
+
+        scoreAdder = initialScore - (float)(Timer_Script.framesLeft * timeMultiplier);
+        score = (uint)initialScore;
+
+        scoreEffects = new Score_Effects(scoreText);
+    }
+    
+
+    void FixedUpdate(){
+
+        Debug.Log(CarController.totalNumChildrenDroppedOff);
+        //make sure that the player doesn't have negative points
+        if ((scoreAdder + ((float)CarController.totalNumChildrenDroppedOff * childrenMultiplier) + ((float)Timer_Script.framesLeft * timeMultiplier)) <= 0)
+            score = 0;
+
+        //apply the points formula
+        else
+            score = (uint) (scoreAdder + ((float)CarController.totalNumChildrenDroppedOff * childrenMultiplier) + ((float)Timer_Script.framesLeft * timeMultiplier));
+        
+        
+        scoreText.text = score.ToString() + "pt";
+    }
+}
--- Assets/Scripts/World Space/Pick_Up_Zone_Collisions.cs
+++ Assets/Scripts/World Space/Pick_Up_Zone_Collisions.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class Pick_Up_Zone_Collisions : MonoBehaviour
 {
-    bool alreadyVisited = false;
+    bool alreadyVisited;
     public uint numChildrenAtPickUp; //number of children at this pick up zone
     public Collider busCollider; //initialize this in the editor to whatever collider the bus is using
 
+    void Awake(){
+
+        //just make sure this variable is initalized to false when the game starts
+        alreadyVisited = false;
+    }
+
+
     //when one of the drop zones detects a collision, double check if it is colliding with the car and then call the Drop_Off function
     void OnTriggerEnter(Collider collider){
+
 
         if (collider == busCollider && alreadyVisited == false){
 
